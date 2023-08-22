a95028eda33e259989ede2a8d4c943a1b754e0d9
mellis <mellis.thedev@gmail.com>
2023-08-22 10:54:31 -0400

    initial file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className='text-3xl font-bold underline'>
+      Sup!
+    </h1>
+  )
+}
+
+export default App
