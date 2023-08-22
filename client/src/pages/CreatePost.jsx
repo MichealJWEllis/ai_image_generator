7ba0c4cefca751f183d165fb418c515ba187476e
mellis <mellis.thedev@gmail.com>
2023-08-22 16:14:05 -0400

    working on getting online

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -33,7 +33,7 @@ const CreatePost = () => {
             try {
                 setGeneratingImg(true);
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/dalle",
+                    "https://aigenerator3.onrender.com/api/v1/dalle",
                     {
                         method: "POST",
                         headers: {
@@ -69,7 +69,7 @@ const CreatePost = () => {
             setLoading(true);
             try {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/post",
+                    "https://aigenerator3.onrender.com/api/v1/post",
                     {
                         method: "POST",
                         headers: {
